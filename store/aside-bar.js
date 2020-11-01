649ac0461b3e111693c0fa2b7833ae1b657cd770
Karol Bartkowski <k.bartkowski@asper.net.pl>
2020-11-01 21:24:18 +0100

    change methods order

--- store/aside-bar.js
+++ store/aside-bar.js
@@ -1,29 +1,32 @@
 export const state = () => ({
   isActive: false,
   asideComponent: null,
-  isAsideComponentReady: false,
-  asideComponentProps: {}
+  asideComponentProps: {},
+  isAsideComponentReady: false
 })
 
 export const mutations = {
+  CHANGE_ASIDE_BAR_ACTIVE_STATE (state, isActive) {
+    state.isActive = isActive
+  },
   TOGGLE_ASIDE_BAR (state) {
     state.isActive = !state.isActive
   },
   CHANGE_ASIDE_COMPONENT (state, component) {
     state.asideComponent = component
   },
-  CHANGE_ASIDE_COMPONENT_READY_STATUS (state, isReady) {
-    state.isAsideComponentReady = isReady
-  },
-  CHANGE_ASIDE_BAR_ACTIVE_STATE (state, isActive) {
-    state.isActive = isActive
-  },
   CHANGE_ASIDE_COMPONENT_PROPS (state, props) {
     state.asideComponentProps = props
+  },
+  CHANGE_ASIDE_COMPONENT_READY_STATUS (state, isReady) {
+    state.isAsideComponentReady = isReady
   }
 }
 
 export const actions = {
+  changeAsideBarActiveState ({ commit }, isActive) {
+    commit('CHANGE_ASIDE_BAR_ACTIVE_STATE', isActive)
+  },
   toggleAsideBar ({ commit }) {
     commit('TOGGLE_ASIDE_BAR')
   },
@@ -36,8 +39,5 @@ export const actions = {
       commit('CHANGE_ASIDE_COMPONENT_PROPS', props)
     }
     commit('CHANGE_ASIDE_COMPONENT_READY_STATUS', true)
-  },
-  changeAsideBarActiveState ({ commit }, isActive) {
-    commit('CHANGE_ASIDE_BAR_ACTIVE_STATE', isActive)
   }
 }
