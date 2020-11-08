9ceaa383ab04d57a0aee45a911bd0e9359278643
Karol Bartkowski <k.bartkowski@asper.net.pl>
2020-11-08 18:00:41 +0100

    adding gallery modal to gallery in sidebar

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row v-if="images" no-gutters>
     <v-col
-      v-for="image in images"
+      v-for="(image, index) in images"
       :key="image.name"
       class="d-flex child-flex"
       cols="4"
@@ -9,9 +9,29 @@
       <v-img
         :src="image.sizes['thumbnail-square']"
         aspect-ratio="1"
-        class="grey lighten-2"
+        @click="openGallery(index)"
       />
     </v-col>
+    <v-dialog
+      v-model="galleryShown"
+      max-width="1280"
+      content-class="bg-dark-gray"
+    >
+      <v-carousel v-model="carouselIndex" height="auto">
+        <v-carousel-item
+          v-for="(image,i) in images"
+          :key="i"
+          eager
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        >
+          <v-img
+            :src="image.sizes['max-image-size']"
+            eager
+          />
+        </v-carousel-item>
+      </v-carousel>
+    </v-dialog>
   </v-row>
 </template>
 
@@ -20,6 +40,21 @@ export default {
   props: {
     // eslint-disable-next-line vue/require-default-prop
     images: Array
+  },
+  data () {
+    return {
+      galleryShown: false,
+      carouselIndex: 0
+    }
+  },
+  methods: {
+    openGallery (index) {
+      const self = this
+      this.carouselIndex = index
+      setTimeout(() => {
+        self.galleryShown = true
+      }, 200)
+    }
   }
 }
 </script>
