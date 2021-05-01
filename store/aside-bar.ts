26cc2310e8678d0b5041158a7a613e7d10ad948b
Karol Bartkowski <k.bartkowski@asper.net.pl>
2021-05-01 13:21:28 +0200

    adding types to app-bar and aside-bar store modules

--- store/aside-bar.ts
+++ store/aside-bar.ts
@@ -1,30 +1,34 @@
+import { ActionTree, MutationTree } from 'vuex'
+
 export const state = () => ({
   isActive: false,
-  asideComponent: null,
+  asideComponent: null as string | null,
   asideComponentProps: {},
   isAsideComponentReady: false
 })
 
-export const mutations = {
-  CHANGE_ASIDE_BAR_ACTIVE_STATE (state, isActive) {
+export type AsideBarState = ReturnType<typeof state>
+
+export const mutations: MutationTree<AsideBarState> = {
+  CHANGE_ASIDE_BAR_ACTIVE_STATE (state, isActive: boolean) {
     state.isActive = isActive
   },
   TOGGLE_ASIDE_BAR (state) {
     state.isActive = !state.isActive
   },
-  CHANGE_ASIDE_COMPONENT (state, component) {
+  CHANGE_ASIDE_COMPONENT (state, component: string) {
     state.asideComponent = component
   },
-  CHANGE_ASIDE_COMPONENT_PROPS (state, props) {
+  CHANGE_ASIDE_COMPONENT_PROPS (state, props: {[key: string]: any}) {
     state.asideComponentProps = props
   },
-  CHANGE_ASIDE_COMPONENT_READY_STATUS (state, isReady) {
+  CHANGE_ASIDE_COMPONENT_READY_STATUS (state, isReady: boolean) {
     state.isAsideComponentReady = isReady
   }
 }
 
-export const actions = {
-  changeAsideBarActiveState ({ commit, state }, isActive) {
+export const actions: ActionTree<AsideBarState, AsideBarState> = {
+  changeAsideBarActiveState ({ commit, state }, isActive: boolean) {
     if (state.isActive !== isActive) {
       commit('CHANGE_ASIDE_BAR_ACTIVE_STATE', isActive)
     }
@@ -32,7 +36,7 @@ export const actions = {
   toggleAsideBar ({ commit }) {
     commit('TOGGLE_ASIDE_BAR')
   },
-  changeAsideComponent ({ commit }, { props, component }) {
+  changeAsideComponent ({ commit }, { props, component }: {props?: {[key: string]: any}, component?: string}) {
     commit('CHANGE_ASIDE_COMPONENT_READY_STATUS', false)
     if (component) {
       commit('CHANGE_ASIDE_COMPONENT', component)
