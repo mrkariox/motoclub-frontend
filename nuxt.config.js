192969f39eacf502db0b4d4e23880a9b012097be
Karol Bartkowski <k.bartkowski@asper.net.pl>
2021-05-03 19:47:57 +0200

    changing meta data and favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - motoclub-frontend',
-    title: 'motoclub-frontend',
+    titleTemplate: '%s - where real journey begins',
+    title: 'Motoclub',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
